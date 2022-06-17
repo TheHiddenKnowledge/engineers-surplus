e9a40f96fb4f2cf3d2072f742cc8596135dcad75
TheHiddenKnowledge <finney538@gmail.com>
2022-06-16 19:57:39 -0700

    Added

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
 });
-server.listen(PORT, () => {
+
+app.listen(PORT, () => {
     console.log(`Server running on ${PORT}/`);
   });
